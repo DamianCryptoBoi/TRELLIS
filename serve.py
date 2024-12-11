eb6db0cf3680ddd7a15f25c32191ac783f19c637
DustinSun <duongnvh.work@gmail.com>
2024-12-12 00:38:04 +0700

    ...

--- serve.py
+++ serve.py
@@ -75,7 +75,8 @@ def image_to_3d(prompt: str, image: Image.Image, ss_guidance_strength: float = 7
             "cfg_strength": slat_guidance_strength,
         },
     )
-    ply_path = outputs['gaussian'][0].save_ply(f"./tmp/{seed}.ply")
+    ply_path = f"./tmp/{seed}.ply"
+    outputs['gaussian'][0].save_ply()
     print("Ply file saved at:", ply_path)
     #read the ply file
     with open(ply_path, "rb") as f:
