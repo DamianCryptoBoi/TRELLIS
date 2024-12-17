6798421dc5e8f6f4aa7c6e95b5288bd39f5cecf8
DustinSun <duongnvh.work@gmail.com>
2024-12-17 17:22:45 +0700

    ...

--- serve.py
+++ serve.py
@@ -20,6 +20,26 @@ from pydantic import BaseModel
 from fastapi.responses import Response, StreamingResponse
 import replicate
 
+import torch
+from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
+from huggingface_hub import hf_hub_download
+from safetensors.torch import load_file
+
+base = "stabilityai/stable-diffusion-xl-base-1.0"
+repo = "ByteDance/SDXL-Lightning"
+ckpt = "sdxl_lightning_4step_unet.safetensors" # Use the correct ckpt for your step setting!
+
+# Load model.
+unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
+unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device="cuda"))
+pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
+
+# Ensure sampler uses "trailing" timesteps.
+pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+
+# Ensure using the same inference steps as the loaded model and CFG set to 0.
+# pipe("A girl smiling", num_inference_steps=4, guidance_scale=0).images[0].save("output.png")
+
 # client = Together()
 
 MAX_SEED = np.iinfo(np.int32).max
@@ -67,17 +87,18 @@ def generate_image(prompt: str):
     # print("Time taken to generate image:", end_time - start_time)
     # return image
 
-    output = replicate.run(
-    "recraft-ai/recraft-20b",
-    input={
-        "size": "1024x1024",
-        "style": "digital_illustration/3d",
-        "prompt": prompt
-        }
-    )
-    image_url = output
-    response = requests.get(image_url)
-    image = Image.open(BytesIO(response.content))
+    # output = replicate.run(
+    # "recraft-ai/recraft-20b",
+    # input={
+    #     "size": "1024x1024",
+    #     "style": "digital_illustration/3d",
+    #     "prompt": prompt
+    #     }
+    # )
+    # image_url = output
+    # response = requests.get(image_url)
+    # image = Image.open(BytesIO(response.content))
+    image = pipe(prompt, num_inference_steps=4, guidance_scale=0).images[0]
     end_time = time.time()
     print("Time taken to generate image:", end_time - start_time)
     return image
