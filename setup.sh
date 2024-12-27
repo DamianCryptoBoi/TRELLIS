428df781dd37dff106502d62f5946f8de3da9075
DustinSun <duongnvh.work@gmail.com>
2024-12-27 11:15:18 +0700

    ...

--- setup.sh
+++ setup.sh
@@ -68,6 +68,7 @@ if [ "$NEW_ENV" = true ] ; then
     conda create -n trellis python=3.10 -y
     conda activate trellis
     conda install -c conda-forge gxx_linux-64 -y
+    conda install cudnn=8.* -c nvidia -c conda-forge -y
     conda install pytorch==2.4.0 torchvision==0.19.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 fi
 
